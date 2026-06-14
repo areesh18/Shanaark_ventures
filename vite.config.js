41c8ab284894d2c39e2db3dd5053ed33dba5abca
Areesh18 <areeshzafar396@gmail.com>
2026-06-14 14:51:37 +0530

    implement caching

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["**/*.{webp,woff2,svg}"],
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,webp,woff2,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: /\.(?:webp|png|jpg|jpeg|svg)$/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images-cache",
+              expiration: {
+                maxEntries: 60,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:js|css|woff2)$/i,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "static-resources",
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
