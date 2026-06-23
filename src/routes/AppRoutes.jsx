e3edea34b27b1526ef2d9288c4ad7b7d9ae27a0e
Areesh18 <areeshzafar396@gmail.com>
2026-06-24 01:40:16 +0530

    Add lazy load blog

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import MainLayout from "../layouts/MainLayout/MainLayout";
 import Home from "../pages/Home/Home";
 import Services from "../pages/Services/Services";
@@ -8,22 +9,24 @@ import Blog from "../pages/Blog/Blog";
 import Careers from "../pages/Careers/Careers";
 import Contact from "../pages/Contact/Contact";
 import About from "../pages/About/About";
-import BlogDetail from "../pages/Blog/BlogDetail";
+const BlogDetail = lazy(() => import("../pages/Blog/BlogDetail"));
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/markets" element={<Markets />} />
-        <Route path="/caribbean" element={<Caribbean />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogDetail />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/markets" element={<Markets />} />
+          <Route path="/caribbean" element={<Caribbean />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogDetail />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
