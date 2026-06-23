d7a4c355b1a08af0145a0c09ac7f46b3289df66d
Areesh18 <areeshzafar396@gmail.com>
2026-06-24 01:48:06 +0530

    Some changes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
+    visualizer({ open: false }),
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
